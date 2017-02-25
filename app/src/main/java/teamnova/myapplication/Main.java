4652d349cf5376aa24d239237f9ee545873241dd
DESKTOP-RHT6QHR\kippe <kipper12335@gmail.com>
2017-02-25 17:20:40 +0900

    no message

---
+++ app/src/main/java/teamnova/myapplication/Main.java
@@ -0,0 +1,16 @@
+package teamnova.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class Main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+    }
+}
