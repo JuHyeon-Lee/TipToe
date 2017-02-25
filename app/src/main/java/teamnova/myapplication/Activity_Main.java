0671785262ec88d42595d55e476d45e238f079e3
jh2510b <leon6095@gmail.com>
2017-02-25 21:23:43 +0900

    스플래시 및 로그인 뷰페이저 구현

--- app/src/main/java/teamnova/myapplication/Activity_Main.java
+++ app/src/main/java/teamnova/myapplication/Activity_Main.java
@@ -1,13 +1,14 @@
 package teamnova.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity_Main extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
